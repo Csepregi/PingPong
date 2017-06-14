6b451914eb658078a2ff129e6db1de08fcbf41f6
Gábor Csepregi <csepregigabor@outlook.com>
2017-06-14 13:04:18 +0200

    First version

--- PingPong/Form1.Designer.cs
+++ PingPong/Form1.Designer.cs
@@ -29,11 +29,70 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Racket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.Ball);
+            this.playground.Controls.Add(this.Racket);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(695, 322);
+            this.playground.TabIndex = 0;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Red;
+            this.Ball.Location = new System.Drawing.Point(171, 84);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(30, 30);
+            this.Ball.TabIndex = 2;
+            this.Ball.TabStop = false;
+            // 
+            // Racket
+            // 
+            this.Racket.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Racket.Location = new System.Drawing.Point(111, 291);
+            this.Racket.Name = "Racket";
+            this.Racket.Size = new System.Drawing.Size(170, 19);
+            this.Racket.TabIndex = 1;
+            this.Racket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 322);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racket)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.PictureBox Racket;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- PingPong/Form1.cs
+++ PingPong/Form1.cs
@@ -12,9 +12,61 @@ namespace PingPong
 {
     public partial class Form1 : Form
     {
+        public int speed_left = 4;  //Speed of the ball
+        public int speed_top = 4;
+        public int point = 0;         // Scored points
+
         public Form1()
         {
             InitializeComponent();
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            Racket.Top = playground.Bottom - (playground.Bottom / 10); // Set the position of the racket
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Racket.Left = Cursor.Position.X - (Racket.Width / 2);  // Set the center of the racket to the position of the cursor
+
+            Ball.Left += speed_left;   //Move the ball
+            Ball.Top += speed_top;
+
+            if (Ball.Bottom >= Racket.Top && Ball.Bottom <= Racket.Bottom && Ball.Left >= Racket.Left && Ball.Right <= Racket.Right)  //Racket collision
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top;  //Change direction
+                point += 1;
+
+            }
+
+            if (Ball.Left <= playground.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if (Ball.Right >= playground.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (Ball.Top <= playground.Top)
+            {
+                speed_top = -speed_top;
+            }
+
+            if (Ball.Bottom >= playground.Bottom)
+                timer1.Enabled = false;  //Ball is out -> Stop the game
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) { this.Close(); } // press escape to quit
         }
     }
 }
+
